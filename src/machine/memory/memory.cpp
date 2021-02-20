30d0791edfe81d57d338bcccbcb2cc5a0b891056
ar43 <47793453+ar43@users.noreply.github.com>
2021-02-20 01:05:45 +0100

    preparing for ppu dev...

--- src/machine/memory/memory.cpp
+++ src/machine/memory/memory.cpp
@@ -4,7 +4,7 @@
 #include "../../utility/utility.h"
 #include <assert.h>
 
-void Memory::WriteCPU(size_t loc, uint8_t byte) //cpu
+void Memory::WriteCPU(size_t loc, uint8_t byte)
 {
 	if (loc < 0 || loc >= cpu_data.size())
 	{ 
@@ -25,7 +25,7 @@ void Memory::WriteCPU(size_t loc, uint8_t byte) //cpu
 	cpu_data[loc] = byte;
 }
 
-uint8_t Memory::ReadCPU(size_t loc) //cpu
+uint8_t Memory::ReadCPU(size_t loc)
 {
 	if (loc < 0 || loc >= cpu_data.size())
 	{ 
@@ -46,6 +46,48 @@ uint8_t Memory::ReadCPU(size_t loc) //cpu
 	return cpu_data[loc];
 }
 
+void Memory::WritePPU(size_t loc, uint8_t byte)
+{
+	if (loc < 0 || loc >= ppu_data.size())
+	{ 
+		logger::PrintLine(logger::LogType::FATAL_ERROR, "Memory::WritePPU - Tried to write out of bounds: " + std::to_string(loc));
+		return;
+	}
+
+	//mirroring
+	if (loc >= 0x3000 && loc <= 0x3EFF)
+	{
+		loc &= 0x2EFF;
+	}
+	else if (loc >= 0x3f20 && loc <= 0x3FFF)
+	{
+		loc &= 0x3f1f;
+	}
+
+	ppu_data[loc] = byte;
+}
+
+uint8_t Memory::ReadPPU(size_t loc)
+{
+	if (loc < 0 || loc >= ppu_data.size())
+	{ 
+		logger::PrintLine(logger::LogType::FATAL_ERROR, "Memory::ReadPPU - Tried to read out of bounds: " + utility::int_to_hex(loc));
+		return 0;
+	}
+
+	//mirroring
+	if (loc >= 0x3000 && loc <= 0x3EFF)
+	{
+		loc &= 0x2EFF;
+	}
+	else if (loc >= 0x3f20 && loc <= 0x3FFF)
+	{
+		loc &= 0x3f1f;
+	}
+
+	return ppu_data[loc];
+}
+
 bool Memory::LoadNES(NesData *nes_data)
 {
 	mapper.number = nes_data->header.mapper_num;
@@ -60,6 +102,8 @@ bool Memory::LoadNES(NesData *nes_data)
 		int start_prg_1 = 0x8000;
 		int start_prg_2 = 0xC000;
 		assert(nes_data->prg_rom.size() <= 2);
+		assert(nes_data->chr_rom.size() == 1);
+		char* dat_chr = nes_data->chr_rom.at(0).get();
 		if (nes_data->prg_rom.size() == 1)
 		{
 			char* dat = nes_data->prg_rom.at(0).get();
@@ -76,8 +120,12 @@ bool Memory::LoadNES(NesData *nes_data)
 			for (int i = 0; i < INES_PRG_BLOCK_SIZE; i++)
 			{
 				cpu_data[start_prg_1 + i] = dat[i];
-				cpu_data[start_prg_1 + i] = dat2[i];
+				cpu_data[start_prg_2 + i] = dat2[i];
 			}
+		}
+		for (int i = 0; i < INES_CHR_BLOCK_SIZE; i++)
+		{
+			ppu_data[i] = dat_chr[i];
 		}
 		break;
 	}
--- src/machine/memory/memory.h
+++ src/machine/memory/memory.h
@@ -19,13 +19,18 @@ public:
 		std::fill(std::begin(cpu_data), std::end(cpu_data), 0);
 	}
 	static const size_t CPU_MEM_SIZE = 0x10000;
+	static const size_t PPU_MEM_SIZE = 0x4000;
 
 	void WriteCPU(size_t loc, uint8_t byte);
 	uint8_t ReadCPU(size_t loc);
+
+	void WritePPU(size_t loc, uint8_t byte);
+	uint8_t ReadPPU(size_t loc);
 
 	bool LoadNES(NesData *nes_data);
 
 private:
 	std::array<uint8_t, Memory::CPU_MEM_SIZE> cpu_data;
+	std::array<uint8_t, Memory::PPU_MEM_SIZE> ppu_data;
 	Mapper mapper;
 };
